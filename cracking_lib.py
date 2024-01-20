ae8448dd90077a25ef0bf360909b38956ac2a1ae
pauel3312 <pau.lacazedieu@gmail.com>
2024-01-20 12:57:59 +0100

    question 1.7

--- cracking_lib.py
+++ cracking_lib.py
@@ -16,3 +16,10 @@ def freq_lettre(s: str, c: str) -> float:
             nb_appartitons_c += 1
 
     return nb_appartitons_c/len(s)
+
+
+def freq_texte(s: str) -> list[float]:
+    frequences = []
+    for caractere in caracteres_chiffres:
+        frequences.append(freq_lettre(s, caractere))
+    return frequences
